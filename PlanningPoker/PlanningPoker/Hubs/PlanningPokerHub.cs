177fe180e05c8288a30cb6ee9338b14121a9860d
Ross Scott <ross.d.scott@gmail.com>
2025-07-14 19:21:34 +0100

    quick hack

--- PlanningPoker/PlanningPoker/Hubs/PlanningPokerHub.cs
+++ PlanningPoker/PlanningPoker/Hubs/PlanningPokerHub.cs
@@ -20,7 +20,7 @@ public class PlanningPokerHub : Hub
 
         if (string.IsNullOrEmpty(accessToken) || !accessToken.Contains(licenceKey))
         {
-            Clients.Caller.SendAsync("LoginFailed", "Invalid licence key. Please log in with a valid licence key.");
+            Clients.Caller.SendAsync("LoginFailed", $"Invalid licence key. Please log in with a valid licence key. {accessToken}  xx  {licenceKey}");
 
             // Reject the connection
             Context.Abort();
